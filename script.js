94194a36b546ed6716c8c2b14cce7f3f8ad0d7c3
gmun <garrettohrt@gmail.com>
2022-09-19 12:40:03 -0500

    add if statements, rename variables, create for loop, add notes

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+function generatePassword() {
+  //Arrays for all of the password character choices
+  var uppercaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "~"];
 
-var result = [];
-var userChoice = [];
+  //Empty arrays will be concatenated and joined to make the final password result
+  var result = [];
+  var userChoice = [];
 
+//Prompts will let the user choose what characters to include in the password
+  var passLength = prompt("Choose a password length between 8 and 128 characters.");
+  var lowercase = confirm("Do you want lowercase in your password?");
+  var uppercase = confirm("Do you want uppercase in your password?");
+  var number = confirm("Do you want lowercase in your password?");
+  var special = confirm("Do you want special characters in your password?");
 
+//If statements used to concatenate the user choices in the empty result array
+  if (lowercase) {
+    result = result.concat(lowercaseSet);
 
-  var passLength = prompt ("Choose a password length between 8 and 128 characters.");
-  var numberConfirm = confirm ("Do you want numbers in your password?");
-  var upperCaseConfirm = confirm ("Do you want uppercase in your password?");
-  var lowerCaseConfirm = confirm ("Do you want lowercase in your password?");
-  var specialConfirm = confirm ("Do you want special characters in your password?");
+  }
 
+  if (uppercase) {
+    result = result.concat(uppercaseSet);
 
+  }
+
+  if (number) {
+    result = result.concat(numbSet);
+
+  }
+
+  if (special) {
+    result = result.concat(specialSet);
+  }
+  console.log(result)
+
+//For loop used to paste the random result array in the empty userChoice array
+  for (var i = 0; i < passLength; i++) {
+
+    userChoice.push(result[Math.floor(Math.random() * result.length)]);
+  }
+
+  //Join method used to turn the array into a string
+  return userChoice.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
